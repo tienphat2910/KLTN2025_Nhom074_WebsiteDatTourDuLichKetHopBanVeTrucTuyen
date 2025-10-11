2f590d04ccb86f3235fa685ef0e911ebc2e9930d
tienphat2910 <21032191.phat@student.iuh.edu.vn>
2025-10-11 19:44:35 +0700

    them thanh toan momo

--- server/index.js
+++ server/index.js
@@ -22,7 +22,6 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs, {
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/tours', require('./routes/tours'));
 app.use('/api/destinations', require('./routes/destinations'));
-app.use('/api/hotels', require('./routes/hotels'));
 app.use('/api/flights', require('./routes/flights'));
 app.use('/api/activities', require('./routes/activities'));
 app.use('/api/airports', require('./routes/airports'));
